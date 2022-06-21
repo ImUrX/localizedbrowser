621858a155c8d41dbbf6f75797c7823cca07a7c3
Uriel <urielfontan2002@gmail.com>
2022-06-20 23:19:42 -0300

    everything works now

--- forge/src/main/java/io/github/interrecipebrowser/forge/mixin/MixinMinecraftClientForge.java
+++ forge/src/main/java/io/github/interrecipebrowser/forge/mixin/MixinMinecraftClientForge.java
@@ -3,6 +3,7 @@ package io.github.interrecipebrowser.forge.mixin;
 import io.github.interrecipebrowser.InterRecipeBrowser;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +13,8 @@ import java.util.stream.Stream;
 
 @Mixin(MinecraftClient.class)
 public class MixinMinecraftClientForge {
-    @Inject(at = @At("TAIL"), method = "method_1502", cancellable = true)
-    private static void mapItemTooltip(ItemStack stack, CallbackInfoReturnable<Stream<String>> cir) {
-        cir.setReturnValue(cir.getReturnValue().map(InterRecipeBrowser::removeDiacritics));
+    @Inject(at = @At("TAIL"), method = "lambda$createSearchTrees$5", cancellable = true)
+    private static void mapItemTooltip(Text text, CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue(InterRecipeBrowser.removeDiacritics(cir.getReturnValue()));
     }
 }
