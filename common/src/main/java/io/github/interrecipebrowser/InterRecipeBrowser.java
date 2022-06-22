c67c04da032be40e85a7e9d52c03cc3772c20c92
ImUrX <urielfontan2002@gmail.com>
2022-06-22 19:29:58 -0300

    we dont need kotlin now

--- common/src/main/java/io/github/interrecipebrowser/InterRecipeBrowser.java
+++ common/src/main/java/io/github/interrecipebrowser/InterRecipeBrowser.java
@@ -11,21 +11,18 @@ import java.text.Normalizer;
 public class InterRecipeBrowser {
     public static final String MOD_ID = "interrecipebrowser";
     private static final Logger LOGGER = LoggerFactory.getLogger("Localized Browser");
-    private static boolean kotlin = false;
 
     public static String simplifyGraphemes(String string) {
-        if(kotlin) {
-            boolean romaji;
-            if ((romaji = Wanakana.isRomaji(string)) || Wanakana.isJapanese(string)) {
-                string = simplifyKana(string);
-                if (romaji) return string;
-            }
+        boolean romaji;
+        if ((romaji = Wanakana.isRomaji(string)) || Wanakana.isJapanese(string)) {
+            string = simplifyKana(string);
+            if (romaji) return string;
         }
         return removeDiacritics(string);
     }
 
     public static String simplifyKana(String string) {
-        return kotlin ? Wanakana.toHiragana(string, IMEMode.TO_HIRAGANA) : string;
+        return Wanakana.toHiragana(string, IMEMode.TO_HIRAGANA);
     }
 
     public static String removeDiacritics(String string) {
@@ -34,13 +31,6 @@ public class InterRecipeBrowser {
     }
 
     public static void init() {
-        try {
-            Class.forName("kotlin.KotlinVersion");
-            kotlin = true;
-        } catch(ClassNotFoundException ex) {
-            kotlin = false;
-            LOGGER.warn("Kotlin wasn't found, Japanese transliteration will be disabled!");
-        }
         LOGGER.info("I now exist");
     }
 }
