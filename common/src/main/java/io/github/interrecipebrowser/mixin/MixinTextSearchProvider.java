8b101add7496ed5ce4e151c295e3ae0722c11c78
ImUrX <urielfontan2002@gmail.com>
2022-06-24 22:00:50 -0300

    changing pc

--- common/src/main/java/io/github/interrecipebrowser/mixin/MixinTextSearchProvider.java
+++ common/src/main/java/io/github/interrecipebrowser/mixin/MixinTextSearchProvider.java
@@ -1,5 +1,6 @@
 package io.github.interrecipebrowser.mixin;
 
+import com.google.common.collect.ObjectArrays;
 import io.github.interrecipebrowser.InterRecipeBrowser;
 import io.github.interrecipebrowser.access.TextSearchProviderAccess;
 import net.minecraft.client.search.SuffixArray;
@@ -10,9 +11,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -32,6 +33,10 @@ public class MixinTextSearchProvider<T> implements TextSearchProviderAccess {
     @Redirect(method = "reload", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/search/TextSearcher;of(Ljava/util/List;Ljava/util/function/Function;)Lnet/minecraft/client/search/TextSearcher;"))
     private TextSearcher<T> onReload(List<T> values, Function<T, Stream<String>> textsGetter) {
         if(this.needsNormalize) {
+            List<T> repeatValues = new ArrayList<>();
+            for (T value : values) {
+                
+            }
             textsGetter = textsGetter.andThen(stream -> stream.map(InterRecipeBrowser::simplifyGraphemes));
         }
         return TextSearcher.of(values, textsGetter);
