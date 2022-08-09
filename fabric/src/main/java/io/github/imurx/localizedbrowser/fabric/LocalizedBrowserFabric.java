c22d2ab6cd5d1be464f753334592936b64e2ab6a
ImUrX <urielfontan2002@gmail.com>
2022-08-09 02:43:34 -0300

    implement fabric keybind

--- fabric/src/main/java/io/github/imurx/localizedbrowser/fabric/LocalizedBrowserFabric.java
+++ fabric/src/main/java/io/github/imurx/localizedbrowser/fabric/LocalizedBrowserFabric.java
@@ -1,12 +1,27 @@
 package io.github.imurx.localizedbrowser.fabric;
 
+import de.siphalor.amecs.api.AmecsKeyBinding;
+import de.siphalor.amecs.api.KeyModifiers;
 import io.github.imurx.localizedbrowser.LocalizedBrowser;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class LocalizedBrowserFabric implements ModInitializer {
+    public final KeyBinding changeLocale = new AmecsKeyBinding(
+            "key.localizedbrowser.locale",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_SPACE,
+            "key.categories.ui",
+            new KeyModifiers(false, true, false)
+    );
+
     @Override
     public void onInitialize() {
-        LocalizedBrowser.init(FabricLoader.getInstance().getConfigDir());
+        LocalizedBrowser.init(FabricLoader.getInstance().getConfigDir(), changeLocale);
+        KeyBindingHelper.registerKeyBinding(this.changeLocale);
     }
 }
