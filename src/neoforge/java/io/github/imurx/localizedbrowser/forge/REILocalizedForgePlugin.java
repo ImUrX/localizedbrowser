c7586e0122e5c15f5b04b1c9f1f0ccbe17977bc3
Uriel <imurx@proton.me>
2025-08-14 19:05:50 -0400

    forgot neoforge part of REI

---
+++ src/neoforge/java/io/github/imurx/localizedbrowser/forge/REILocalizedForgePlugin.java
@@ -0,0 +1,8 @@
+package io.github.imurx.localizedbrowser.forge;
+
+import io.github.imurx.localizedbrowser.compat.rei.REILocalizedPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
+
+@REIPluginClient
+public class REILocalizedForgePlugin extends REILocalizedPlugin {
+}
